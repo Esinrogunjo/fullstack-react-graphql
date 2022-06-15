fbbff123eccaf5fe9efe0233c8eeb1df491863a0
Dauda Adbulganiyu <daudakale@gmail.com>
2022-06-15 18:52:33 +0100

    project add

---
+++ client/src/queries/projectQueries.js
@@ -0,0 +1,13 @@
+import { gql } from "@apollo/client";
+
+const GET_PROJECTS = gql`
+  query getProjects {
+    projects {
+      name
+      id
+      status
+    }
+  }
+`;
+
+export { GET_PROJECTS };
